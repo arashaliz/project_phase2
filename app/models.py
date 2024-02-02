fce0d60edbc3f6de01124895e875882946d304a2
AliHaji <alihaj2858@gmail.com>
2024-02-02 23:58:57 +0330

    created user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
+from django.contrib.admin import site
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class District(models.Model):
+    location = models.CharField(max_length=255)
+
+
+class User(AbstractUser):
+    is_patient = models.BooleanField(default=True)
+    is_doctor = models.BooleanField(default=False)
+    is_clinic_manager = models.BooleanField(default=False)
+    district = models.ForeignKey(District, on_delete=models.deletion.SET_NULL, null=True, blank=True)
+
